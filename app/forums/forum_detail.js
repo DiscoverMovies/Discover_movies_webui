89324e207ddbbbcb422f871a8506a31cb80399b0
sherinann <sherinannthomas1@gmail.com>
2017-06-07 22:36:44 +0530

    changed allignments

--- app/forums/forum_detail.js
+++ app/forums/forum_detail.js
@@ -9,7 +9,7 @@ $routeProvider.when('/forums/:id',{
     }])
 
 
-.controller('forumDetailsCtrl', function($scope, $http,$routeParams,$rootScope){
+.controller('forumDetailsCtrl', function($scope, $http,$routeParams,$rootScope,$location){
     var id=$routeParams.id;
     $.get('http://localhost:8080/forum/get/'+id).then(function(response){
         var forumDetail = response['forum'];
@@ -20,4 +20,33 @@ $routeProvider.when('/forums/:id',{
     }, function(resp){
         console.log(resp);
     })
+    $.get('http://localhost:8080/forum/replies/'+id).then(function(response){
+        //var replyDetail = response['forum'];
+        //$scope.forumDetail=forumDetail;
+        //$scope.$apply();
+        console.log(response);
+    
+    }, function(resp){
+        console.log(resp);
+    })
+
+   $scope.replyAddForum= function(){
+       console.log('reply create called');
+        console.log(id);
+        var token=$rootScope.token;
+        var text=$scope.replyPost;
+        console.log(token,text);
+       $.post('http://localhost:8080/forum/reply/post/'+id,
+        {
+           token:token,
+           text:text
+      },
+      function(data, status){
+      if(data.status=="OK"){
+       console.log('replied');
+        $location.path('/forums/');
+      }
+      });
+   }
+    
 })
